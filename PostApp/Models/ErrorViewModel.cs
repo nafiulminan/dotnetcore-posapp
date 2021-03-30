600693faf531c52d6b53c89e8c8d5e946fb0b381
Nafiul Minan A <fadhil.nma@gmail.com>
2021-03-30 13:34:21 +0700

    add master dotnet core

---
+++ PostApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PostApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
